1fd9d3235dce3fa0447b7ba2ead2ff2e15b6247e
Landon Harvey <landonharvey5@gmail.com>
2018-11-27 19:41:32 -0600

    to master

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@ import budheader from '../components/budheader.vue';
 import sidebar from '../components/sidebar.vue';
 import purchase from '../components/purchase.vue';
 import budgetandFinancialsContainer from '../components/budgetandFinancialsContainer.vue';
-
+git
 @Component({
   components: {
         budheader,
